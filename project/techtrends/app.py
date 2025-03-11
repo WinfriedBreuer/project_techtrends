2f15776a0be3cd7d9bb52243b5ea8769d0557b50
Winfried Breuer <mail@winfried-breuer.de>
2025-03-11 21:18:36 +0100

    added database.db and added healthcheck, metrics and logs to flask application app.py

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,14 +1,30 @@
 import sqlite3
+import logging
+import sys
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
+db_connection_count = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+
+    global db_connection_count
+    db_connection_count += 1
+
     return connection
+
+# Function to get number of posts in DB
+def get_no_posts():
+    connection = get_db_connection()
+    no_posts = connection.execute('SELECT COUNT(DISTINCT ID) FROM posts',
+                        ).fetchone()
+    connection.close()
+    return int(no_posts[0])
 
 # Function to get a post using its ID
 def get_post(post_id):
@@ -21,6 +37,26 @@ def get_post(post_id):
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
+
+@app.route('/healthz')
+def healthz():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"db_connection_count": db_connection_count, "post_count": get_no_posts()}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
 
 # Define the main route of the web application 
 @app.route('/')
@@ -36,13 +72,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        app.logger.warning('Article with id "{}" does not exist'.format(post_id))
+        return render_template('404.html'), 404      
     else:
+      app.logger.info('Article "{}" retrieved'.format(post['title']))
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('About Us page retrieved')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -58,8 +97,10 @@ def create():
             connection = get_db_connection()
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
                          (title, content))
-            connection.commit()
+            connection.commit()            
             connection.close()
+
+            app.logger.info('Article "{}" created'.format(title))
 
             return redirect(url_for('index'))
 
@@ -67,4 +108,8 @@ def create():
 
 # start the application on port 3111
 if __name__ == "__main__":
+   
+   #set up logging
+   logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(asctime)s - %(message)s')
+
    app.run(host='0.0.0.0', port='3111')
